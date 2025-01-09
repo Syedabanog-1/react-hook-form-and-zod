947305a45a6d468b66e5e4ff8c038ec602514e0a
syedabanog-1 <gulzar_bano@yahoo.com>
2025-01-09 12:29:09 +0500

    commit hoga ha good

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -11,60 +11,60 @@ function Form() {
   } = useForm<FormData>();
 
   const onSubmit = async (data: FormData) => {
-      console.log("SUCCESS", data);
-  }
+    // Form submit hone par alert message show karega
+    alert("Form Submitted Successfully!");
+    console.log("SUCCESS", data);
+  };
 
   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid col-auto">
-          <h1 className="text-3xl font-bold mb-4">
-            Zod & React-Hook-Form
-          </h1>
-          <FormField
-            type="email"
-            placeholder="Email"
-            name="email"
-            register={register}
-            error={errors.email}
-          />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid col-auto">
+        <h1 className="text-3xl font-bold mb-4">Zod & React-Hook-Form</h1>
+        <FormField
+          type="email"
+          placeholder="Email"
+          name="email"
+          register={register}
+          error={errors.email}
+        />
 
-          <FormField
-            type="text"
-            placeholder="GitHub URL"
-            name="githubUrl"
-            register={register}
-            error={errors.githubUrl}
-          />
+        <FormField
+          type="text"
+          placeholder="GitHub URL"
+          name="githubUrl"
+          register={register}
+          error={errors.githubUrl}
+        />
 
-          <FormField
-            type="number"
-            placeholder="Years of Experience (1 - 10)"
-            name="yearsOfExperience"
-            register={register}
-            error={errors.yearsOfExperience}
-            valueAsNumber
-          />
+        <FormField
+          type="number"
+          placeholder="Years of Experience (1 - 10)"
+          name="yearsOfExperience"
+          register={register}
+          error={errors.yearsOfExperience}
+          valueAsNumber
+        />
 
-          <FormField
-            type="password"
-            placeholder="Password"
-            name="password"
-            register={register}
-            error={errors.password}
-          />
+        <FormField
+          type="password"
+          placeholder="Password"
+          name="password"
+          register={register}
+          error={errors.password}
+        />
 
-          <FormField
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPassword"
-            register={register}
-            error={errors.confirmPassword}
-          />
-          <button type="submit" className="submit-button">
-            Submit
-          </button>
-        </div>
-      </form>
+        <FormField
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          register={register}
+          error={errors.confirmPassword}
+        />
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </div>
+    </form>
   );
 }
 
