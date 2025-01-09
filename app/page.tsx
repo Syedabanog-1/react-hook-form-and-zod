5ef8315f4e7b4f3c72a7a3b8f3cc0640e4de151a
syedabanog-1 <gulzar_bano@yahoo.com>
2025-01-09 12:25:07 +0500

    commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Form from "./components/Form";
+
+function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hello!</h1>
+     <Form />
+    </main>
+  );
+}
+
+export default Home;
+
